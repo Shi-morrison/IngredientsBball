566376860417bbed42311b3f9e96b0b4cb1b9c83
Shi-morrison <shian.morrison@gmail.com>
2023-12-27 18:12:55 -0500

    Intial Commit

---
+++ frontend/bball/components/header.tsx
@@ -0,0 +1,10 @@
+
+export default function Header() {
+    return (
+        <main>
+            <h1>HI</h1>
+            <h2>BYE</h2>
+            <p>hi</p>
+        </main>
+    )
+}
---
+++ frontend/bball/pages/_document.tsx
@@ -0,0 +1,27 @@
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+
+class MyDocument extends Document {
+    static async getInitialProps(ctx: DocumentContext) {
+        const initialProps = await Document.getInitialProps(ctx);
+        return { ...initialProps };
+    }
+
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+                    <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@500&family=Lexend+Deca:wght@700&display=swap" rel="stylesheet" />
+                    {/* Other head elements can be added here */}
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+
+export default MyDocument;
